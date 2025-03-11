cf1c80d7350d96e56585e6a5f1eb644560f070c6
akshays236 <akshays23623@gmail.com>
2025-03-11 21:34:27 +0530

    Added spotify api credentials

---
+++ config.py
@@ -0,0 +1,7 @@
+# Spotify API credentials
+CLIENT_ID = "8f9536beecb44a1791ea6f175b815fe3"
+CLIENT_SECRET = "a29f3b9edeb3431a8de38c93a2ce258f"
+REDIRECT_URI = "https://example.org/callback"
+
+# Other configurations
+SCOPE = "user-library-read playlist-modify-public"
