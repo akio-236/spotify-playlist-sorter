043735e89bbb7dbc9542ac4099f4b8b20fe6b609
akshays236 <akshays23623@gmail.com>
2025-03-11 22:03:50 +0530

    added the main file

---
+++ main.py
@@ -0,0 +1,28 @@
+from spotify_auth import authenticate_spotify
+from fetch_songs import fetch_liked_songs, fetch_song_metadata
+from organize_playlists import organize_by_genre, create_playlists
+from utils import setup_logging
+
+
+def main():
+    # Set up logging
+    setup_logging()
+
+    # Authenticate with Spotify
+    sp = authenticate_spotify()
+
+    # Fetch liked songs
+    liked_songs = fetch_liked_songs(sp)
+
+    # Fetch song metadata
+    song_data = fetch_song_metadata(sp, liked_songs)
+
+    # Organize songs by genre
+    genre_playlists = organize_by_genre(song_data)
+
+    # Create playlists
+    create_playlists(sp, genre_playlists)
+
+
+if __name__ == "__main__":
+    main()
