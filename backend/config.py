5257652717a9dad86c047d6f225cc295e72d672d
akshays236 <akshays23623@gmail.com>
2025-03-19 10:22:52 +0530

    Setting up spotify credentials

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,9 @@
+import os
+
 # Spotify API credentials
-CLIENT_ID = "8f9536beecb44a1791ea6f175b815fe3"
-CLIENT_SECRET = "a29f3b9edeb3431a8de38c93a2ce258f"
-REDIRECT_URI = "https://example.org/callback"
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")  # Read from environment variable
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")  # Read from environment variable
+REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI", "http://localhost:8888/callback")
 
 # Other configurations
 SCOPE = "user-library-read playlist-modify-public"
