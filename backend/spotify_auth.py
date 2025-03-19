c0c0a92d92136a2fe177a87d947f7c223593c193
akshays236 <akshays23623@gmail.com>
2025-03-19 10:30:08 +0530

    Refactor Spotify authentication function to return SpotifyOAuth object directly

--- backend/spotify_auth.py
+++ backend/spotify_auth.py
@@ -1,16 +1,13 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from backend.config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPE
+from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPE
 
 
 def authenticate_spotify():
-    """Authenticate with Spotify and return a Spotify object."""
-    sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            redirect_uri=REDIRECT_URI,
-            scope=SCOPE,
-        )
+    """Authenticate with Spotify and return a SpotifyOAuth object."""
+    return SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPE,
     )
-    return sp
